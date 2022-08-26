26225a1244458f2d99deedde7efe0d2078d70538
jimagrini <juan.magrini@correo.ucu.edu.uy>
2022-08-26 19:51:35 -0300

    Subiendo desafio

---
+++ Desafio/src/Desafio1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Library;
+Sucesion Uno = new Sucesion();
+Uno.Calculator(1000);
---
+++ Desafio/src/Library/Sucesion.cs
@@ -0,0 +1,38 @@
+﻿namespace Library;
+public class Sucesion
+{
+    public void Calculator(int valor)
+    {
+        int LongestCadena=0;
+        foreach (var value in Enumerable.Range(1, valor).Reverse())
+        {
+            Console.WriteLine($"---Numero actual: {value} ---");
+            int Pasos= 0;
+            int Result = value;
+            while(Result!=1)
+            {
+                if(Result%2==0)
+                {
+                    Result= Result/2;
+                    Console.WriteLine($"{Result}");
+                    Pasos+=1;
+                    
+                }
+                else
+                {
+                    Result=(3*Result)+1;
+                    Console.WriteLine($"{Result}");
+                    Pasos+=1;
+                }
+
+            }
+            if(Pasos>LongestCadena)
+            {
+                LongestCadena=value;
+                Pasos=0;
+            }
+            Console.WriteLine("----------------");
+        }
+        Console.WriteLine($"La cadena más larga la produce el número {LongestCadena}");    
+    }
+}
